32eab1c7ea0a663edeaa42d6bd80f4e15a18949f
Iman Behroozifar <105700046+imanbehroozifar@users.noreply.github.com>
2023-04-21 11:12:27 +0430

    Connecting the project to hygraph, writing queries and mutations

---
+++ src/graphql/query.js
@@ -0,0 +1,77 @@
+import { gql } from "@apollo/client";
+
+const GET_BLOGS_INFO = gql`
+
+    query{
+        posts{
+            author{
+                name
+                avatar{
+                    url
+                }
+            }
+            title
+            id
+            slug
+            coverPhoto{
+                url
+            }
+        }
+    }
+
+`;
+const GET_AUTHORS_INFO = gql`
+    query{
+        authors{
+            id
+            name
+            slug
+            avatar{url}
+        }
+    }   
+`;
+
+const GET_AUTHOR_INFO = gql`
+    query getAuthorInfo($slug:String!){
+        author(where:{slug:$slug}){
+            avatar{url}
+            field
+            name
+            description{html}
+            posts{
+                coverPhoto{url}
+                id {}
+                slug
+                title
+            }
+        }
+    }
+`;
+const GET_POST_INFO = gql`
+    query getPostInfo($slug:String!){
+        author{
+            avatar{url}
+            name
+            field
+        }
+        content{
+            html
+        }
+        title
+        coverPhoto{url}
+
+    }
+
+`;
+const GET_COMMENT = gql`
+    query getPostComment($slug:String!){
+        comment(where:{post:{slug:$slug}}){
+            id
+            name
+            email
+        }
+    }
+`
+
+export {GET_BLOGS_INFO, GET_AUTHOR_INFO, GET_POST_INFO, GET_AUTHOR_INFO}
+
